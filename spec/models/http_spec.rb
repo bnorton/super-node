25c365dab4410485884af271a7073417c2ab19dd
Brian Norton <brian.nort@gmail.com>
2012-02-16 02:47:58 -0800

    Adding GET and https for calls out to external services SSL/https is only used

--- spec/models/http_spec.rb
+++ spec/models/http_spec.rb
@@ -2,31 +2,43 @@ require 'spec_helper'
 
 describe SuperNode::HTTP do
   let(:http) { mock(Net::HTTP) }
-  let(:post) { mock(Net::HTTP::Post) }
   let!(:uri) { URI.parse("https://callback.endpoint.com/now") }
+  
+  before do
+    URI.stub(:parse).and_return(uri)
+  end
 
-  describe "#post" do
-    before do
-      # Net::HTTP.stub(:new).and_return(http)
-      # Net::HTTP::Post.stub(:new).and_return(post)
-      URI.stub(:parse).and_return(uri)
-    end
+  shared_examples_for "a connectable interface" do
     it "should post with the correct uri" do
       Net::HTTP.should_receive(:new).with(uri.host, 443).and_return(http)
       http.should_receive(:use_ssl=).with(true)
       http.should_receive(:verify_mode=).with(OpenSSL::SSL::VERIFY_NONE)
-      Net::HTTP::Post.should_receive(:new).with(uri.request_uri).and_return(post)
-      http.should_receive(:request).with(post)
+      "Net::HTTP::#{type.to_s.capitalize}".constantize.should_receive(:new).with(uri.request_uri).and_return(conn)
+      http.should_receive(:request).with(conn)
 
-      SuperNode::HTTP.new("https://callback.endpoint.com/now").post
+      SuperNode::HTTP.new("https://callback.endpoint.com/now").send(type)
     end
 
     it "should make a network connection" do
-      stub_request(:post, "https://callback.endpoint.com/now").
+      stub_request(type, "https://callback.endpoint.com/now").
          with(:headers => {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
          to_return(:status => 200, :body => "", :headers => {})
 
-      SuperNode::HTTP.new("https://callback.endpoint.com/now").post
+      SuperNode::HTTP.new("https://callback.endpoint.com/now").send(type)
     end
+  end
+
+  describe "#post" do
+    let!(:conn) { mock(Net::HTTP::Post) }
+    def type; :post; end
+
+    it_behaves_like "a connectable interface"    
+  end
+
+  describe "#get" do
+    let!(:conn) { mock(Net::HTTP::Get) }
+    def type; :get; end
+
+    it_behaves_like "a connectable interface"
   end
 end
